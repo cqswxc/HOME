8c6653a35d7bb1a81c74f2ee27eb64ca10d43bdc
cqswxc <66579679+cqswxc@users.noreply.github.com>
2020-12-18 15:25:07 +0800

    Add files via upload

---
+++ js/testIsoto.js
@@ -0,0 +1,29 @@
+// // 全部
+// $('#grid').isotope({ filter: '*' });
+// class 中包含 transition
+// $('#grid').isotope({ filter: '.transition' });
+// // class 中包含 metal
+// $('#grid').isotope({ filter: '.metal' });
+
+// jQuery 代码过滤
+// $('#grid').isotope({
+//   filter: function() {
+//     var number = $(this).find('.number').text();
+//     return parseInt( number, 10 ) > 50;
+//   }
+// })
+
+function maintest() {
+	$('#grid').isotope({
+		filter: "*"
+	});
+}
+
+maintest()
+$('.button-group').on('click', 'button', function() {
+	var filterValue = $(this).attr('data-filter');
+	$('#grid').isotope({
+		filter: filterValue
+	});
+});
+
